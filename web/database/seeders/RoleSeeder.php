9a2a9464c0ab451068458e569b105dc75c5201b7
Albin Varghese <albin8285@gmail.com>
2024-04-13 10:55:15 +0530

    feat: create RoleSeeder

---
+++ web/database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create roles
+        $role = Role::create(['name' => 'user']);
+
+        // role that is assigned to campus entities like  college canteen, library, store, etc.
+        $role = Role::create(['name' => 'campus entity']);
+    }
+}
